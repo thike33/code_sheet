0a6f7e02f94f645bbcf0d500fce46f6df5b6dee7
thike33 <teke3business@gmail.com>
2024-07-19 19:44:35 +0900

    feat:TailwindCSSのカラー設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,15 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main-color': '#A1D6E2', // light blue
+        'sub-color': '#1995AD', // blue
+        'bg-color': '#F1F1F2', // light grey
+        'footer-color': '#BCBABE', // grey
+      },
+    },
+  },
 }
