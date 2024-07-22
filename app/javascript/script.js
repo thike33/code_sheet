d76dac0edd8f99998e897278988c29fab7d6c6c7
thike33 <teke3business@gmail.com>
2024-07-22 16:02:53 +0900

    feat:ログイン後のメニュー開閉機能実装

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,6 +1,23 @@
-const headerBtn = document.querySelector(".js-header-btn");
-const headerMenu = document.querySelector(".js-header-menu");
-headerBtn.addEventListener("click", () => {
-  headerBtn.classList.toggle("is-open");
-  headerMenu.classList.toggle("is-open");
+// PC時のユーザーメニュー
+document.addEventListener("turbo:load", () => {
+  const body = document.body;
+  const isLoggedIn = body.getAttribute('data-logged-in') === 'true';
+  if(isLoggedIn) {
+    const headerUserBtn = document.querySelector(".js-user-btn");
+    const headerUserMenu = document.querySelector(".js-user-menu");
+    headerUserBtn.addEventListener("click", () => {
+      headerUserBtn.classList.toggle("is-open");
+      headerUserMenu.classList.toggle("is-open");
+    });
+  }
+});
+
+// SP時のヘッダーメニュー
+document.addEventListener("turbo:load", () => {
+  const headerBtn = document.querySelector(".js-header-btn");
+  const headerMenu = document.querySelector(".js-header-menu");
+  headerBtn.addEventListener("click", () => {
+    headerBtn.classList.toggle("is-open");
+    headerMenu.classList.toggle("is-open");
+  });
 });
