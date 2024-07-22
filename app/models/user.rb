3e9d0ea336fb3ef0dfc717399b8b1b3e52097b7a
thike33 <teke3business@gmail.com>
2024-07-22 17:29:25 +0900

    fix:usersテーブルとのリレーションが無かったため、postsテーブルを新しく作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 100 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
