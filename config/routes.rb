7b591a4a5375be7471a3216fdf7f730e2f454fb4
thike33 <teke3business@gmail.com>
2024-07-23 09:57:36 +0900

    feat:postsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "static_pages#top"
+  resources :posts, only: %i[index]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
