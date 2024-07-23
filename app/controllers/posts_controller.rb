5154fbbcd3a59303ff673f4b0973806025f60211
thike33 <teke3business@gmail.com>
2024-07-23 21:44:08 +0900

    feat:新規投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(created: :desc)
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 end
