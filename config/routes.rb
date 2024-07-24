c54e08303ee3c09b0d4f4c7b093185943014dca6
thike33 <teke3business@gmail.com>
2024-07-24 17:34:28 +0900

    feat:詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "static_pages#top"
-  resources :posts, only: %i[index new create]
+  resources :posts, only: %i[index new create show]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
