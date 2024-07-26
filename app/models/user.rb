ea204e74ee1c629295ad8896d790c1982baaa9c6
thike33 <teke3business@gmail.com>
2024-07-26 19:48:37 +0900

    feat:avatarの設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :avatar, AvatarUploader
+
   def own?(object)
     id == object&.user_id
   end
