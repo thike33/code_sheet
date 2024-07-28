d0c2b0fed6f538ae866a3a7ffba0ba7a144c7db4
thike33 <teke3business@gmail.com>
2024-07-28 16:51:32 +0900

    feat:投稿のフラッシュメッセージ追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,9 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path
+      redirect_to posts_path, success: t('defaults.flash_message.created', item: Post.model_name.human)
     else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Post.model_name.human)
       render :new, status: :unprocessable_entity
     end
   end
@@ -27,8 +28,9 @@ class PostsController < ApplicationController
   def update
     @post = current_user.posts.find(params[:id])
     if @post.update(post_params)
-      redirect_to post_path(@post)
+      redirect_to post_path(@post), success: t('defaults.flash_message.updated', item: Post.model_name.human)
     else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: Post.model_name.human)
       render :edit, status: :unprocessable_entity
     end
   end
@@ -36,7 +38,7 @@ class PostsController < ApplicationController
   def destroy
     @post = current_user.posts.find(params[:id])
     @post.destroy!
-    redirect_to posts_path, status: :see_other
+    redirect_to posts_path, success: t('defaults.flash_message.destroyed', item: Post.model_name.human), status: :see_other
   end
 
   def user_index
