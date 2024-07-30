79819f34fcdb21e5c2a445a9d99b3589e90bd15f
thike33 <teke3business@gmail.com>
2024-07-30 21:24:45 +0900

    feat:投稿のフォームと詳細にコードを反映

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # 指定された属性名と値を人間が読みやすい形式の属性名を返す
+  def self.human_attribute_enum_value(attr_name, value)
+    return if value.blank?
+    human_attribute_name("#{attr_name}.#{value}")
+  end
+
+  # 指定された列挙型の属性名に基づき、その選択肢をハッシュに変換
+  def self.enum_options_for_select(attr_name)
+    self.send(attr_name.to_s.pluralize).map { |k, _| [self.human_attribute_enum_value(attr_name, k), k] }.to_h
+  end
 end
