7165b6827cea13e39de34f7a244e761d316278d9
thike33 <teke3business@gmail.com>
2024-08-17 14:25:42 +0900

    feat:投稿フォームのコードを追加・削除する機能を作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :codes, dependent: :destroy
-  accepts_nested_attributes_for :codes
+  accepts_nested_attributes_for :codes, allow_destroy: true
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 100 }
