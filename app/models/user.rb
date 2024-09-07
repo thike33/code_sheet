f934f93066873c932016cdb2419ab7dd514ed1f1
thike33 <teke3business@gmail.com>
2024-09-07 10:59:17 +0900

    feat:likeモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_posts, through: :bookmarks, source: :post
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 
   validates :name, presence: true, length: { maximum: 100 }
   # Include default devise modules. Others available are:
