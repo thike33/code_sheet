4af8ef96c49dd5519dc98d1a95572b68429f2e8b
thike33 <teke3business@gmail.com>
2024-10-09 21:38:37 +0900

    feat:tagモデルとpost_tagsモデル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   has_many :codes, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
   accepts_nested_attributes_for :codes, allow_destroy: true
   belongs_to :user
 
